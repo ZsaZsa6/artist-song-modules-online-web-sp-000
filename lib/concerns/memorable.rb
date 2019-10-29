56214684cf1de98646666b28d4a97321cd17fba2
Ronda D Cowen <cowenhome5@gmail.com>
2019-10-29 21:09:31 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -3,11 +3,15 @@ module Memorable
   module ClassMethods
 
     def reset_all
-        all.clear
+        self.all.clear
     end
 
     def count
-        all.count
+        self.all.count
     end
+  end
+
+  module InstanceMethods
+    
   end
 end
