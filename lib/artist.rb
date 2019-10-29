c3dc7741193d46d201149456765d5ec4967cd2ec
Ronda D Cowen <cowenhome5@gmail.com>
2019-10-29 21:11:45 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,6 +2,7 @@ require 'pry'
 
 class Artist
   extend Memorable::ClassMethods
+  include Memorable:InstanceMethods
   extend Findable::ClassMethods
   include Paramable::InstanceMethods
   attr_accessor :name
@@ -10,7 +11,7 @@ class Artist
   @@artists = []
 
   def initialize
-    self.class.all << self
+    super
     @songs = []
   end
 
